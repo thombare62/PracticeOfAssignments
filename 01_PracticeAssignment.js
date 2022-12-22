1c6c4573dbc3bcb630d25c3a31107bd55a858e7f
thombare62 <thombare62@gmail.com>
2022-12-22 21:37:14 +0530

    Create 01_PracticeAssignment.js

---
+++ 01_PracticeAssignment.js
@@ -0,0 +1,7 @@
+var myName //Variable declaration 
+myName="Chandrashekhar Bharat Thombare" //Variable initialisation
+console.log("My full name is:" ,myName); //Log on console
+
+
+var myName="Chandrashekhar Thombare" //Variable declaration & initialisation on same line {Mainly preferred/Followed}
+console.log("My name is:" ,myName);
