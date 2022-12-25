07bb565d5dd27185f6b46b2ab0798e8fd6266d49
thombare62 <thombare62@gmail.com>
2022-12-25 21:14:08 +0530

    First assignment
    This assignment includes variable declaration, initialisation and logging on the console.

---
+++ 01_variable_assign.js
@@ -0,0 +1,22 @@
+var myFullName
+console.log(myFullName);
+myFullName=("Chandrashekhar Thombare");
+console.log(myFullName);
+myFullName=("Chandrashekhar Bharat Thombare");
+console.log(myFullName);
+var myLove=("I love only JS");
+console.log(myLove);
+var firstName=("Chandrashekhar");
+console.log(firstName);
+var lastName=("Thombare");
+console.log(lastName);
+var age=26
+console.log(age);
+var adress= ("At Masalwadi post Loni Bhapkar Taluka Baramati District Pune.");
+console.log(adress);
+var pin_code=(400709);
+console.log(pin_code);
+var gender=("Male");
+console.log(gender);
+var is_married=("No, there's so much time to get married")
+console.log(is_married);
