77252bdc41dfa8872e17ad282e463f401fcc3232
thombare62 <thombare62@gmail.com>
2023-01-30 18:54:47 +0530

    assignment 122 and scope file

--- 22_controlFlow.js
+++ 22_controlFlow.js
@@ -1,46 +1,74 @@
 console.log(`To find vowels in given string: \n`);
-string= "I am very good IT Developer"
-count=0
+string = "I am very good IT Developer";
+count = 0;
 for (let index = 0; index < string.length; index++) {
-    var eachChar= string.charAt(index)
-    let eachCharInLow= eachChar.toLowerCase()
-    // console.log(eachCharInLow);
-    if (eachCharInLow== "a" || eachCharInLow=="e" || eachCharInLow== "i" || eachCharInLow== "o" || eachCharInLow== "u"  ) {
-        count=1+count
-    }
+  var eachChar = string.charAt(index);
+  let eachCharInLow = eachChar.toLowerCase();
+  // console.log(eachCharInLow);
+  if (
+    eachCharInLow == "a" ||
+    eachCharInLow == "e" ||
+    eachCharInLow == "i" ||
+    eachCharInLow == "o" ||
+    eachCharInLow == "u"
+  ) {
+    count = 1 + count;
+  }
 }
 console.log(`Given string ==> ${string}`);
 console.log(`The total number of vowels in the given string ==> ${count}`);
-console.log(`==================================================================`);
+console.log(
+  `==================================================================`
+);
 console.log(`To find sum of cubes from 1 to 5: \n`);
-function sumOfCubes(){
-counter= 0;
-for (let index = 1; index <=5; index++) {
-    let multi= index*index*index
+function sumOfCubes() {
+  counter = 0;
+  for (let index = 1; index <= 5; index++) {
+    let multi = index * index * index;
     //console.log(multi);
-    counter= counter+multi
+    counter = counter + multi;
+  }
+  console.log(`The sum of cubes from 1 to 5 ==> ${counter}`);
 }
-console.log(`The sum of cubes from 1 to 5 ==> ${counter}`);
-}
-sumOfCubes()
-console.log(`==================================================================`);
+sumOfCubes();
+console.log(
+  `==================================================================`
+);
 console.log(`To find odd positioned characters from a given string: \n`);
 
-var oddPositionedChar = function (string) {
-    var stringLength=string.length
-    var newStr = ""
-    for (let index = 0; index < stringLength; index++) {
-      if (index%2==1) {
-        oddChar = string.charAt(index);
-        newStr = newStr.concat(oddChar);
-        newStr = newStr.split("").join("")
-      }
+function oddPositionedChars(str) {
+  let result = "";
+
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] !== " " && i % 2 !== 0) {
+      result = result + str[i];
     }
-    console.log(`The given string is= "${string}"`);
-    console.log(`Odd positioned characters without considering space is= "${newStr}"`);
   }
-  oddPositionedChar("Hard work always pays back");
-  oddPositionedChar("Soon i will be Angular IT champ");
+  console.log(result);
+}
 
+const string1 = "Hard work always pays back";
+const string2 = "Soon I will be Angular IT Champ";
 
+oddPositionedChars(string1);
+oddPositionedChars(string2);
 
+console.log(
+  `==================================================================`
+);
+console.log(`TO LOG VOWELS FROM A GIVEN STRING`);
+let str = "Hello world im back";
+revStr = "";
+for (let i = 0; i < str.length; i++) {
+  newStr = str.charAt(i);
+  if (
+    newStr == "a" ||
+    newStr == "e" ||
+    newStr == "i" ||
+    newStr == "o" ||
+    newStr == "u"
+  ) {
+    revStr = revStr.concat(newStr);
+  }
+}
+console.log(revStr);
